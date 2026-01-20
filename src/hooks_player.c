5533866664fb1b55d8390f5cf01979e21df05d9d
LucasCodeur <l.dadamopro@gmail.com>
2026-01-20 11:28:07 +0100

    FIX: movement on the side, move correctly according orientation

--- src/hooks_player.c
+++ src/hooks_player.c
@@ -6,7 +6,7 @@
 /*   By: lud-adam <lud-adam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 15:27:25 by lud-adam          #+#    #+#             */
-/*   Updated: 2026/01/20 09:57:00 by lud-adam         ###   ########.fr       */
+/*   Updated: 2026/01/20 11:27:40 by lud-adam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,15 @@
 #include <mlx.h>
 #include <X11/keysym.h>
 
-static	void move_left_or_right(t_data *data, double to_rotate);
+static	void move_left_or_right(t_data *data, bool right);
 
 bool	move_hero(t_data *data)
 {
+
+	printf("dir value x : %f\n", data->map.player.dir.elements[0]);
+	printf("dir value y : %f\n", data->map.player.dir.elements[1]);
+	// printf("pos value x : %f\n", data->map.player.pos.elements[0]);
+	// printf("pos value y : %f\n", data->map.player.pos.elements[1]);
 	if (data->keycode.down == true && 
 		data->map.grid[(int)(data->map.player.pos.elements[1] - SPEED * data->map.player.dir.elements[1])][(int)(data->map.player.pos.elements[0] - SPEED * data->map.player.dir.elements[0])] == '0')
 	{
@@ -35,50 +40,34 @@ bool	move_hero(t_data *data)
 		data->map.player.pos.elements[1] += SPEED * data->map.player.dir.elements[1];
 	}
 	else if (data->keycode.left == true)
-	{
-		if (data->map.player.dir.elements[0] != -1 && data->map.player.dir.elements[1] != 0)
-		{
-			move_left_or_right(data, ROTATE_BACKWARD);
-			printf("backward\n");
-		}
-		else
-		{
-			printf("forward\n");
-			move_left_or_right(data, ROTATE_FORWARD);
-		}
-	}
+			move_left_or_right(data, false);
 	else if (data->keycode.right == true)
-	{
-		if (data->map.player.dir.elements[0] != 0 && data->map.player.dir.elements[1] != -1)
-		{
-			printf("forward\n");
-			move_left_or_right(data, ROTATE_FORWARD);
-		}
-		else
-		{
-			printf("backward\n");
-			move_left_or_right(data, ROTATE_BACKWARD);
-		}
-	}
+			move_left_or_right(data, true);
 	return (true);
 }
 
-static	void move_left_or_right(t_data *data, double to_rotate)
+static	void move_left_or_right(t_data *data, bool right)
 {
-	t_vec dir_rotate;
+	double side_x;
+	double side_y;
 
-	dir_rotate = rotate_vect(data->map.player.dir, to_rotate);
-	if (data->map.grid[(int)(data->map.player.pos.elements[1] + SPEED * dir_rotate.elements[1])][(int)(data->map.player.pos.elements[0] + SPEED * dir_rotate.elements[0])] == '0')	
+	side_x = -data->map.player.dir.elements[1];
+	side_y = data->map.player.dir.elements[0];
+	if (right == true && data->map.grid[(int)(data->map.player.pos.elements[1] + data->map.player.plane.elements[1] * SPEED)][(int)(data->map.player.pos.elements[0] + data->map.player.plane.elements[0] * SPEED)] == '0')
 	{
-		data->map.player.pos.elements[0] += SPEED * dir_rotate.elements[0];
-		data->map.player.pos.elements[1] += SPEED * dir_rotate.elements[1];
+		data->map.player.pos.elements[0] += data->map.player.plane.elements[0] * SPEED;
+		data->map.player.pos.elements[1] += data->map.player.plane.elements[1] * SPEED;
 	}
-	else
-		return ;
+	else if (right == false && data->map.grid[(int)(data->map.player.pos.elements[1] - data->map.player.plane.elements[1] * SPEED)][(int)(data->map.player.pos.elements[0] - data->map.player.plane.elements[0] * SPEED)] == '0')
+	{
+		data->map.player.pos.elements[0] -= data->map.player.plane.elements[0] * SPEED;
+		data->map.player.pos.elements[1] -= data->map.player.plane.elements[1] * SPEED;
+	}
 }
 
 bool	rotate_hero(t_data *data)
 {
+	if (data->)
 	if (data->keycode.rotate_left == true)
 	{
 		data->map.player.dir = rotate_vect(data->map.player.dir, SUBT_SPEED);
