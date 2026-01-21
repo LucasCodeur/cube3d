84affdd9f1acf6c9469050cf1d31c60fa6f05f06
Pierre-Antoine Rigaudier <prigaudi@z2r10p1.42lyon.fr>
2026-01-21 16:59:54 +0100

    add Error before error message

--- .obj/draw/utils_draw.d
+++
@@ -1,8 +0,0 @@
-.obj/draw/utils_draw.o: src/draw/utils_draw.c inc/display.h inc/error.h \
-  src//math/inc/vector.h
-
-inc/display.h:
-
-inc/error.h:
-
-src//math/inc/vector.h:
--- .obj/minimap_utils.d
+++
@@ -1,8 +0,0 @@
-.obj/minimap_utils.o: src/minimap_utils.c inc/display.h inc/error.h \
-  src//math/inc/vector.h
-
-inc/display.h:
-
-inc/error.h:
-
-src//math/inc/vector.h:
