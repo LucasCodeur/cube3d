9cf368f1b8f8b877c4a2a3a44f30191d9cf5f4cf
Pierre-Antoine Rigaudier <prigaudi@z2r10p1.42lyon.fr>
2026-01-21 15:20:03 +0100

    norme

--- .obj/fps.d
+++
@@ -1,7 +0,0 @@
-.obj/fps.o: src/fps.c inc/display.h inc/error.h src//math/inc/vector.h
-
-inc/display.h:
-
-inc/error.h:
-
-src//math/inc/vector.h:
--- src/math/.obj/vector/divide_vector_operation.d
+++
@@ -1,4 +0,0 @@
-.obj/vector/divide_vector_operation.o: \
-  src/vector/divide_vector_operation.c inc/vector.h
-
-inc/vector.h:
