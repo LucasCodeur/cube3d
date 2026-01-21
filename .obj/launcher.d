3ffdb907c785caf1ccbe110b82ecbfcf00795be0
Pierre-Antoine Rigaudier <prigaudi@z2r10p1.42lyon.fr>
2026-01-21 16:59:46 +0100

    add Error before error message

---
+++ .obj/launcher.d
@@ -0,0 +1,12 @@
+.obj/launcher.o: src/launcher.c inc/display.h inc/error.h \
+  src//math/inc/vector.h inc/parsing.h minilibx-linux/mlx.h
+
+inc/display.h:
+
+inc/error.h:
+
+src//math/inc/vector.h:
+
+inc/parsing.h:
+
+minilibx-linux/mlx.h:
