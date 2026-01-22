aa0a05f84f294b34b2e9094ebc484b7648716cbf
Pierre-Antoine Rigaudier <prigaudi@z1r9p5.42lyon.fr>
2026-01-22 16:52:35 +0100

    fix norme headers

--- src/math/.obj/vector/magnitude_vector.d
+++
@@ -1,4 +0,0 @@
-.obj/vector/magnitude_vector.o: src/vector/magnitude_vector.c \
-  inc/vector.h
-
-inc/vector.h:
--- src/math/.obj/vector/vector.d
+++
@@ -1,3 +0,0 @@
-.obj/vector/vector.o: src/vector/vector.c inc/vector.h
-
-inc/vector.h:
