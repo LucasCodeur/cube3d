cd13430e012631a3b2e8625a37baaa7755f7ab4e
LucasCodeur <l.dadamopro@gmail.com>
2026-01-28 16:23:37 +0100

    FEAT: creation class bureaucrat in canonical form

---
+++ cpp/cpp_05/inc/Bureaucrat.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lud-adam <lud-adam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/28 15:42:31 by lud-adam          #+#    #+#             */
+/*   Updated: 2026/01/28 16:20:39 by lud-adam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <string>
+#include <iostream>
+
+class Bureaucrat
+{
+	private:
+		const std::string 	_name;
+		unsigned short int	_grade;
+	public:
+		Bureaucrat(void);
+		Bureaucrat(std::string const &name, unsigned short int _grade);
+		Bureaucrat(Bureaucrat &other);
+		~Bureaucrat(void);
+		Bureaucrat& operator=(const Bureaucrat &other);
+
+		const std::string & getName() const;
+		unsigned short int getGrade() const;
+};
+
+#endif
---
+++ cpp/cpp_05/src/Bureaucrat.cpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lud-adam <lud-adam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/28 15:52:31 by lud-adam          #+#    #+#             */
+/*   Updated: 2026/01/28 16:20:06 by lud-adam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(void) : _name("John Doe"), _grade(150)
+{
+	std::cout << "Default Constructor of Bureaucrat called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string &_name, unsigned short int _grade) : _name(_name), _grade(_grade)
+{
+	std::cout << "Parameterized Constructor of Bureaucrat called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat &other) : _name(other._name), _grade(other._grade)
+{
+	std::cout << "Copy Constructor of Bureaucrat called" << std::endl;
+}
+
+Bureaucrat::~Bureaucrat(void)
+{
+	std::cout << "Deconstructor of Bureaucrat called" << std::endl;
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat &other)
+{
+	if (this != &other)
+	{
+		this->_grade = other._grade;
+	}
+	return (*this);
+}
+
+const std::string &Bureaucrat::getName(void) const
+{
+	return (this->_name);
+}
+
+unsigned short int Bureaucrat::getGrade() const
+{
+	return (this->_grade);
+}
