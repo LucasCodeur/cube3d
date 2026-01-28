58e62c63a20af2017b4a41c677f26d81861f36f8
LucasCodeur <l.dadamopro@gmail.com>
2026-01-28 16:39:56 +0100

    FEAT: test parameters constructor

--- cpp/cpp_05/src/main.cpp
+++ cpp/cpp_05/src/main.cpp
@@ -6,14 +6,21 @@
 /*   By: lud-adam <lud-adam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 16:07:41 by lud-adam          #+#    #+#             */
-/*   Updated: 2026/01/28 16:08:18 by lud-adam         ###   ########.fr       */
+/*   Updated: 2026/01/28 16:39:29 by lud-adam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+void	create_bureaucrat_by_parameters(std::string name, unsigned short int grade)
+{
+	Bureaucrat	test(name, grade);
+	std::cout << test.getName() << std::endl;
+	std::cout << test.getGrade() << std::endl;
+}
+
 int	main(void)
 {
-	Bureaucrat	test;
+	create_bureaucrat_by_parameters("George", 150);
 	return (1);
 }
